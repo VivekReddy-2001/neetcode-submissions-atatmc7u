ab3d7e2aafc89f3e8bc7deff4d6443d746bacba2
Vivek Reddy Arrabothu <142435794+VivekReddy-2001@users.noreply.github.com>
2026-05-11 10:59:00 -0400

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.py
@@ -0,0 +1,16 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        s_count=defaultdict(int)
+        t_count=defaultdict(int)
+
+        for i in s:
+            s_count[i]+=1
+        for i in t:
+            t_count[i]+=1
+        
+        if s_count==t_count:
+            return True
+        return False
